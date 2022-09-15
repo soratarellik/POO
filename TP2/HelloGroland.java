e355b03d50c21d8f60400ac5d852c89330d3e665
Ramos Luis <luismbonito@gmail.com>
2022-09-15 16:52:43 +0200

    TP1 ?

---
+++ TP2/HelloGroland.java
@@ -0,0 +1,7 @@
+public class HelloGroland {
+    public static void main(String[] args){
+        System.out.println("Hello Groland");
+        System.out.println(Math.sqrt(25));
+    }    
+}
+
---
+++ TP2/Point.java
@@ -0,0 +1,23 @@
+
+public record Point(int x, int y) {
+	
+	double distance(Point B) {
+		double x;
+		double y;
+		x = Math.pow(B.x - this.x,2);
+		y = Math.pow(B.y - this.y,2);
+		return Math.sqrt(x + y);
+	}
+	
+	public static void main(String args[]) {
+		int x,y;
+		var origin = new Point(0,0);
+		x = Integer.parseInt(args[0]);
+		y = Integer.parseInt(args[1]);
+		System.out.printf("x=%d, y=%d\n",x,y);
+		var p = new Point(x,y);
+		System.out.print(p+"\n");
+		System.out.println(p.distance(origin));
+	}
+  
+}
