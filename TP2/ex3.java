a19c9da12c6a0aa6482c1859efc79206e982f0d1
Ramos Luis <luismbonito@gmail.com>
2022-09-19 22:54:59 +0200

    Exercice 3.3

--- TP2/ex3.java
+++ TP2/ex3.java
@@ -4,8 +4,9 @@ import java.util.regex.Pattern;
 
 public class ex3 {
 
-    public adresse(String ip){
-
+    public String adresse(String ip){
+        var pattern.compile("[0-255]");
+        return "oui"
     }
 
     public static void main(String[] args){
@@ -14,7 +15,7 @@ public class ex3 {
          [0-9]+||.+[0-9]+ pour les chiffres et les arguments qui finissent par des chiffres
         S*/
         String test = "172.16.254.4";
-        
+
         var pattern = Pattern.compile("[0-9]+||.+[0-9]+");
         for(String i : args){
             if(pattern.matcher(i).matches() == true){
