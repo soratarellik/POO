da61294c09ee44b79bd3f23ff9f36a3c7d03b9f9
Ramos Luis <luismbonito@gmail.com>
2022-09-19 22:41:31 +0200

    Exercice 3.3

--- TP2/ex3.java
+++ TP2/ex3.java
@@ -3,11 +3,22 @@ import java.util.regex.Pattern;
 
 
 public class ex3 {
+
+    public adresse(String ip){
+
+    }
+
     public static void main(String[] args){
-        var pattern = Pattern.compile("[0-9]+");
+        /* 
+         [0-9]+ pour les chiffees
+         [0-9]+||.+[0-9]+ pour les chiffres et les arguments qui finissent par des chiffres
+        S*/
+        String test = "172.16.254.4";
+        
+        var pattern = Pattern.compile("[0-9]+||.+[0-9]+");
         for(String i : args){
             if(pattern.matcher(i).matches() == true){
-                System.out.println("Oui c'est ce qu'il me fallait");
+                System.out.println(i);
             }
         }
     }
